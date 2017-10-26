75d8d25d8209e966003bd598fa5beae854136ec0
Miguel Jardón <migueljp83@gmail.com>
2017-10-26 19:23:02 +0200

    Cambio de peso en imágenes y sistema de onClick.

--- app/src/main/java/com/ammyt/guedr/MainActivity.java
+++ app/src/main/java/com/ammyt/guedr/MainActivity.java
@@ -5,8 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     protected static String TAG = MainActivity.class.getCanonicalName();
 
@@ -18,28 +19,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final ImageView offlineImage = findViewById(R.id.offline_weather_image);
+
         changeToStone = findViewById(R.id.change_stone_system);
-        changeToStone.setOnClickListener(this);
+        changeToStone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "PIEDRA");
+                offlineImage.setImageResource(R.drawable.offline_weather);
+            }
+        });
 
         changeToDonkey = findViewById(R.id.change_donkey_system);
-        changeToDonkey.setOnClickListener(this);
+        changeToDonkey.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.v(TAG, "BURRO");
+                offlineImage.setImageResource(R.drawable.tardis);
+            }
+        });
+
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-    }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.change_stone_system:
-                Log.v(TAG, "Han llamado a STONE");
-            case R.id.change_donkey_system:
-                Log.v(TAG, "Han llamado a DONKEY");
-            default:
-                Log.v(TAG, "Han llamado a ??");
-        }
     }
 }
